39875b5fbdf0a8d78803919e6317fa1d7c26e033
godlockit <102461883+godlockit@users.noreply.github.com>
2024-05-13 03:52:54 +0300

    Add files via upload

---
+++ anim/js 16.11.2023/script.js
@@ -0,0 +1,32 @@
+let clot = document.getElementById("clot");
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click",()=>{
+    let hex_num = ["1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    let hexcode = "";
+    let hexcode2 = "";
+    let hexcode3 = "";
+    for(let i = 0; i<6;i++){
+        let random_index = Math.floor(Math.random()*hex_num.length);
+        hexcode +=hex_num[random_index];
+
+    }
+    for(let i = 0; i<6;i++){
+        let random_index = Math.floor(Math.random()*hex_num.length);
+        hexcode2 +=hex_num[random_index];
+
+    }
+    for(let i = 0; i<6;i++){
+        let random_index = Math.floor(Math.random()*hex_num.length);
+        hexcode3 +=hex_num[random_index];
+
+    }
+    clot.innerHTML = hexcode;
+    document.body.style.background = "#"+hexcode;
+    btn.style.background = "#" + hexcode;
+    btn.style.borderColor = "#" + hexcode2;
+    btn.style.color = "#" + hexcode3;
+    ho1.style.color = "#" + hexcode3;
+    clot.style.color = "#" + hexcode3;
+    main.style.borderColor="#"+ hexcode2;
+})
