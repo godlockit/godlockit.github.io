0d2e7c1d0cdc64e407649a2969511efaa2c1d682
godlockit <102461883+godlockit@users.noreply.github.com>
2024-05-13 03:48:05 +0300

    Add files via upload

---
+++ верстки/zapchasti/script.js
@@ -0,0 +1,13 @@
+let input = document.querySelectorAll("#ol")
+let car = document.querySelector(".car")
+function lol( ){
+    for(let i = 0;i < input.length; i++){
+        if (input[i].checked == true){
+            let a = document.createElement("p")
+        }
+        else{
+            console.log
+        }
+     
+    }
+}
